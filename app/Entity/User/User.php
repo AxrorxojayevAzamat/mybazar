fd99bfe3c57128afb815567feb9eed252a8344df
abdurahmonbek <abdurahmonkodirov97@gmail.com>
2020-04-21 15:02:22 +0500

    moved User and fixed its migration

---
+++ app/Entity/User/User.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity\User;
+
+use Carbon\Carbon;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $last_name
+ * @property string $email
+ * @property string $phone
+ * @property bool $phone_verified
+ * @property string $password
+ * @property integer $balance
+ * @property string $verify_token
+ * @property string $phone_verify_token
+ * @property Carbon $phone_verify_token_expire
+ * @property boolean $phone_auth
+ * @property string $role
+ * @property string $status
+ */
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    public const STATUS_WAIT = 0;
+    public const STATUS_ACTIVE = 9;
+
+    const ROLE_USER = 'user';
+    const ROLE_MODERATOR = 'moderator';
+    const ROLE_ADMIN = 'administrator';
+
+    protected $fillable = [
+        'name', 'email', 'phone', 'password', 'verify_token', 'status', 'balance', 'role',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token'
+    ];
+
+    protected $casts = [
+        'phone_verified' => 'boolean',
+        'phone_verify_token_expire' => 'datetime',
+        'phone_auth' => 'boolean',
+    ];
+
+    public static function rolesList(): array
+    {
+        return [
+            self::ROLE_USER => 'User',
+            self::ROLE_MODERATOR => 'Moderator',
+            self::ROLE_ADMIN => 'Admin',
+        ];
+    }
+
+
+    public function isWait(): bool
+    {
+        return $this->status === self::STATUS_WAIT;
+    }
+
+    public function isActive(): bool
+    {
+        return $this->status === self::STATUS_ACTIVE;
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,10 +18,16 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone')->unique();
+            $table->boolean('phone_verified');
+            $table->string('password');
+            $table->bigInteger('balance')->default(0);
+            $table->string('verify_token');
+            $table->string('phone_verify_token');
+            $table->timestamp('phone_verify_token_expire');
+            $table->boolean('phone_auth');
             $table->integer('role');
             $table->integer('status');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
