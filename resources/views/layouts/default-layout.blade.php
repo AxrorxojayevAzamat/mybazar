54b87c87a72a52b24140ae4a2a458a2b95f23028
Malika <malikajuraeva98@gmail.com>
2020-05-29 16:22:14 +0500

    change place of common-styles

--- resources/views/layouts/default-layout.blade.php
+++ resources/views/layouts/default-layout.blade.php
@@ -48,7 +48,14 @@
                 animation: loader-inner 2s infinite ease-in;
             }
         </style>
-        @include ('includes.common-style')
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/mmenu.css')}}">
+        <link rel="stylesheet" href="{{asset('css/animate-3.7.2.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/mybazar-fonts.css')}}">
+        <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+        <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+
+        @yield('styles')
     </head>
     <body>
         <!-- page loader -->
