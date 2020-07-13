e238678970a09da08812dc1bff953175d4aee6ef
Malika <malikajuraeva98@gmail.com>
2020-07-13 14:49:01 +0500

    index-blogs

--- resources/views/layouts/index-blog.blade.php
+++ resources/views/layouts/index-blog.blade.php
@@ -3,6 +3,7 @@
         @foreach($blogs as $blog)
         <a href="{{route('blogs.show',$blog)}}">
             <div class="item first">
+                <img src="/storage/blogs/{{$blog->file}}" alt="">
                 <p class="sub-title">{{$blog->title}}</p>
                 <h5>{{$blog->description}}</h5>
             </div>
