96758937cf617e83107b8839b04d970e5777eb11
kodirov257 <abdurahmonkodirov97@gmail.com>
2020-09-16 14:45:30 +0500

    fixed adminlte

--- resources/views/catalog/catalog.blade.php
+++ resources/views/catalog/catalog.blade.php
@@ -1,56 +1,55 @@
 @extends('layouts.default-layout')
 
-@section('title', 'Catalog page')   
-@include ('includes.common-style')
+@section('title', 'Catalog page')
+    @include ('includes.common-style')
+
     @section('styles')
         <link rel="stylesheet" href="{{asset('css/catalog-page.css')}}">
     @endsection
-        @section('body')
-            @extends ('partials.menu')
-                @section('page')
-                    @section('banner')
-                    <!-- Slide banner -->
-                    @include ('layouts.slide-banner-catalog')
+
+    @section('body')
+        @extends('partials.menu')
+
+        @section('page')
+            @section('banner')
+
+            <!-- Slide banner -->
+            @include ('layouts.slide-banner-catalog')
+        @endsection
+
+        <!-- CATALOG VIEW -->
+        <section>
+            <div class="h4-title catalog-view">
+                <h4 class="title">Телевизоры</h4>
+            </div>
+            <div class="outter-catalog-view">
+                <!-- big filter without title checkbox -->
+                @include('filters.big-filter-without-title-checkbox')
+
+                <div class="wrapper-filtered-items">
+
+                    <nav class=" navbar navbar-expand-custom sort-types">
+
+                        @include('layouts.sort-by-options')
+
+                        @include('filters.small-filter-without-title-checkbox')
+                    </nav>
+
+                    @include('layouts.list-mosaic-catalog-items')
+
+                    @include('layouts.pagination')
+
+                </div>
+            </div>
+        </section>
+    @endsection
 @endsection
-                    <!-- CATALOG VIEW -->
-                    <section>
-                        <div class="h4-title catalog-view">
-                            <h4 class="title">Телевизоры</h4>
-                        </div>
-                        <div class="outter-catalog-view">
-                            <!-- big filter without title checkbox -->
-                            @include('filters.big-filter-without-title-checkbox')
 
-                            <div class="wrapper-filtered-items">
+@section('script')
+    <script src="{{asset('js/autoNumeric-2.0-BETA.js')}}"></script>
+    <script src="{{asset('js/autoNumeric.js')}}"></script>
 
-                                <nav class=" navbar navbar-expand-custom sort-types">
-
-                                    <!--sort-by options  -->
-                                    @include('layouts.sort-by-options')
-                                    
-                                    <!-- small filter without title checkbox -->
-                                    @include('filters.small-filter-without-title-checkbox')
-                                </nav>
-
-                                <!-- list mosaic catalog items -->
-                                @include('layouts.list-mosaic-catalog-items', ['products'=>$product])
-
-                                <!-- pagination -->
-                                @include('layouts.pagination')
-                                
-                            </div>
-                        </div>
-                    </section>
-
-                @endsection
-
-        @endsection
-
-        @section('script')
-            <script src="{{asset('js/autoNumeric-2.0-BETA.js')}}"></script>
-            <script src="{{asset('js/autoNumeric.js')}}"></script>
-            
-            <script src="{{asset('js/1-index.js')}}"></script>
-            <script src="{{asset('js/range-slider.js')}}"></script>
-            <script src="{{asset('js/2-catalog-page.js')}}"></script>
-        @endsection
+    <script src="{{asset('js/1-index.js')}}"></script>
+    <script src="{{asset('js/range-slider.js')}}"></script>
+    <script src="{{asset('js/2-catalog-page.js')}}"></script>
+@endsection
