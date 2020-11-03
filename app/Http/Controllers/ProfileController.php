c95f0faa3f574cf6bce87b3d601cd360602b5bea
shuhrat78jon <shuhrat78jon@gmail.com>
2020-11-03 16:40:40 +0500

    move UserController functions to UserService

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,78 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Entity\User\Profile;
 use App\Entity\User\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
-use App\Helpers\ImageHelper;
+use App\Http\Requests\Admin\Stores\Users\UpdateRequest;
 use App\Services\Sms\SmsSender;
-use Illuminate\Support\Facades\Hash;
 use App\Helpers\JsonHelper;
 use App\Services\User\PhoneService;
 use App\Http\Requests\User\PhoneVerifyRequest;
 use App\Http\Requests\User\PhoneRequest;
 use App\Http\Requests\User\PasswordRequest;
 use Illuminate\Http\Response;
+use App\Services\User\UserService;
 
 class ProfileController extends Controller
 {
 
     private $sms;
     private $service;
+    private $userService;
 
-    public function __construct(SmsSender $sms, PhoneService $service) {
+    public function __construct(SmsSender $sms, PhoneService $service, UserService $userService) {
         $this->middleware('can:manage-profile');
-        $this->sms     = $sms;
-        $this->service = $service;
+        $this->sms         = $sms;
+        $this->service     = $service;
+        $this->userService = $userService;
     }
 
     public function index() {
         $user    = Auth::user();
         $genders = Profile::gendersList();
-        
+
         return view('user.setting', compact('user', 'genders'));
     }
 
-    public function edit(User $user) {
-
-
-        return view('user.edit', compact('user', 'genders'));
-    }
-
-    public function update(Request $request, User $user) {
-
-
-        if (!$request->avatar) {
-            $user->profile->edit($request->first_name, $request->last_name, $request->birth_date, $request->gender, $request->address);
-        } else {
-            Storage::disk('public')->deleteDirectory('/files/' . ImageHelper::FOLDER_PROFILES . '/' . $user->id);
-
-            $imageName = ImageHelper::getRandomName($request->avatar);
-            $user->profile->edit($request->first_name, $request->last_name, $request->birth_date, $request->gender, $request->address, $imageName);
-
-            $this->uploadPoster($user->id, $request->avatar, $imageName);
-        }
+    public function update(UpdateRequest $request, User $user) {
+        $user = $this->service->updateProfile($user->id, $request);
 
         return redirect()->route('user.setting');
     }
 
     public function changePassword(PasswordRequest $request) {
         try {
-
-            if (!(Hash::check($request->get('current_password'), Auth::user()->password))) {
-//            // The passwords matches
-                return JsonHelper::response(Response::HTTP_BAD_REQUEST, 'Your current password does not matches with the password you provided. Please try again.');
-            }
-
-            if (strcmp($request->get('current_password'), $request->get('new_password')) == 0) {
-//            //Current password and new password are same
-
-                return JsonHelper::response(Response::HTTP_BAD_REQUEST, 'New Password cannot be same as your current password. Please choose a different password.');
-            }
-            //Change Password
-            $user           = Auth::user();
-            $user->password = bcrypt($request->get('new_password'));
-            $user->save();
-            return JsonHelper::successResponse('Password changed successfully !');
+            return $this->userService->changePassword($request);
         } catch (\Exception $e) {
             return JsonHelper::response(Response::HTTP_BAD_REQUEST, $e->getMessage());
         }
---
+++ app/Services/User/UserService.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Entity\User\User;
+use App\Helpers\ImageHelper;
+use Illuminate\Http\UploadedFile;
+use App\Http\Requests\Admin\Users\CreateRequest;
+use App\Http\Requests\Admin\Users\UpdateRequest;
+use App\Http\Requests\User\PasswordRequest;
+use Illuminate\Http\Request;
+use App\Entity\User\Profile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Hash;
+use App\Helpers\JsonHelper;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class UserService
+{
+
+    public function findUsers(Request $request) {
+        /** @var User $user */
+        $query = User::orderByDesc('id');
+
+        if (!empty($value = $request->get('id'))) {
+            $query->where('id', $value);
+        }
+
+        if (!empty($value = $request->get('name'))) {
+            $query->where('name', 'like', '%' . $value . '%');
+        }
+
+        if (!empty($value = $request->get('email'))) {
+            $query->where('email', 'like', '%' . $value . '%');
+        }
+
+        if (!empty($value = $request->get('status'))) {
+            $query->where('status', $value);
+        }
+
+        if (!empty($value = $request->get('role'))) {
+            $query->where('role', $value);
+        }
+        return $query;
+    }
+
+    public function store(CreateRequest $request) {
+        /** @var User $user */
+        $user = User::new($request['name'], $request['email'], $request['role'], $request['password']);
+        if ($this->isProfile($request)) {
+            $imageName = null;
+            if ($request->avatar) {
+                $imageName = ImageHelper::getRandomName($request->avatar);
+                $this->uploadAvatar($user->id, $request->avatar, $imageName);
+            }
+            Profile::new($user->id, $request->first_name, $request->last_name, $request->birth_date, $request->gender, $request->address, $imageName);
+        }
+        return $user;
+    }
+
+    public function update($id, UpdateRequest $request) {
+        /** @var User $user */
+        $user = User::findOrFail($id);
+
+        $user->edit($request->name, $request->email, $request->role, $request->status, $request->password);
+        if ($user->profile) {
+
+            $this->updateProfile($user->id, $request);
+        } else {
+            $imageName = null;
+            if ($request->avatar) {
+                $imageName = ImageHelper::getRandomName($request->avatar);
+                $this->uploadAvatar($user->id, $request->avatar, $imageName);
+            }
+            Profile::new($user->id, $request->first_name, $request->last_name, $request->birth_date, $request->gender, $request->address, $imageName);
+        }
+        return $user;
+    }
+
+    public function updateProfile($id, UpdateRequest $request): void {
+        /** @var User $user */
+        $user = User::findOrFail($id);
+
+        if (!$request->avatar) {
+            $user->profile->edit($request->first_name, $request->last_name, $request->birth_date, $request->gender, $request->address);
+        } else {
+            Storage::disk('public')->deleteDirectory('/files/' . ImageHelper::FOLDER_PROFILES . '/' . $user->id);
+
+            $imageName = ImageHelper::getRandomName($request->avatar);
+            $user->profile->edit($request->first_name, $request->last_name, $request->birth_date, $request->gender, $request->address, $imageName);
+
+            $this->uploadAvatar($user->id, $request->avatar, $imageName);
+        }
+    }
+
+    public function changePassword(PasswordRequest $request) {
+       
+
+            if (!(Hash::check($request->get('current_password'), Auth::user()->password))) {
+//            // The passwords matches
+                return JsonHelper::response(Response::HTTP_BAD_REQUEST, 'Your current password does not matches with the password you provided. Please try again.');
+            }
+
+            if (strcmp($request->get('current_password'), $request->get('new_password')) == 0) {
+//            //Current password and new password are same
+
+                return JsonHelper::response(Response::HTTP_BAD_REQUEST, 'New Password cannot be same as your current password. Please choose a different password.');
+            }
+            //Change Password
+            $user           = Auth::user();
+            $user->password = bcrypt($request->get('new_password'));
+            $user->save();
+            return JsonHelper::successResponse('Password changed successfully !');
+        
+    }
+
+    private function uploadAvatar(int $userId, UploadedFile $file, string $imageName): void {
+        ImageHelper::saveThumbnail($userId, ImageHelper::FOLDER_PROFILES, $file, $imageName);
+        ImageHelper::saveOriginal($userId, ImageHelper::FOLDER_PROFILES, $file, $imageName);
+    }
+
+    private function isProfile(CreateRequest $request): bool {
+        $count = 0;
+        $request['first_name'] ? $count++ : 0;
+        $request['last_name'] ? $count++ : 0;
+        $request['birth_date'] ? $count++ : 0;
+        $request['gender'] != Profile::GENDER_EMPTY ? $count++ : 0;
+        $request['address'] ? $count++ : 0;
+        $request['avatar'] ? $count++ : 0;
+        return $count;
+    }
+
+}
