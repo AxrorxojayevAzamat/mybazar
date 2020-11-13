fa61061ccb09964ee8fe00046b368faa5baf5d05
abdurahmonbek <abdurahmonkodirov97@gmail.com>
2020-11-13 11:49:14 +0500

    removed blog categories viewsw]

--- resources/views/admin/blog/categories/create.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.admin.page')
-
-@section('content')
-    <form method="POST" action="{{ route('admin.blog.categories.store') }}">
-        @csrf
-
-        @include('admin.blog.categories._form', ['category' => null])
-    </form>
-@endsection
