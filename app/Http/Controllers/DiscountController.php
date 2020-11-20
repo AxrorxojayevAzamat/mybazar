a5c511f620ee8bc51bb3ec95bb2bc5de297e623e
elnurrabbimov <elnurrabbimov@gmail.com>
2020-11-20 18:28:54 +0500

    some changes

--- app/Http/Controllers/DiscountController.php
+++ app/Http/Controllers/DiscountController.php
@@ -17,7 +17,7 @@ class DiscountController extends Controller {
     public function show(Discount $discount) {
         $recentProducts = Product::orderByDesc('created_at')->limit(8)->get();
         $product = Product::where(['main_category_id' => $discount->category_id])->where('discount', '>', 0)->where('discount_ends_at', '>', date('Y-m-d H:i:s'))
-            ->orderByDesc('discount')->get();
+            ->orderByDesc('discount')->paginate(12);
 
         return view('discounts.show', compact('discount','recentProducts','product'));
     }
