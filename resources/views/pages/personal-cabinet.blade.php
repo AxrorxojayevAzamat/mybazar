e41602667b50a29dc33a97604fc1f8d7d7301e2a
Malika Juraeva <malikajuraeva98@gmail.com>
2020-11-26 12:35:16 +0500

    some changes

---
+++ resources/views/pages/personal-cabinet.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Auth page')
+
+@section('styles')
+    {{-- <link rel="stylesheet" href="{{asset('css/auth.css')}}"> --}}
+@endsection
+
+@section('body')
+    
+
+@endsection
+
+
+@section('script')
+    <script src="{{mix('js/1-index.js', 'build')}}"></script>
+@endsection
