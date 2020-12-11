46c2c3f3dba4d70da88f304c3b23db1cc0f48835
azamat <axrorxojayevazamat@gmail.com>
2020-12-11 16:50:16 +0500

    fix

--- resources/views/layouts/slide-banner-catalog.blade.php
+++ resources/views/layouts/slide-banner-catalog.blade.php
@@ -1,9 +1,10 @@
 <section>
     <div class="slide-banner">
-        @if ($longBanner)
+{{--        {{dd($longBanner)}}--}}
+
+    @if ($longBanner)
             <a href="{{ $longBanner->url }}"><img src="{{ $longBanner->fileCustom }}" alt=""></a>
         @endif
-{{--        {{dd($banner)}}--}}
 {{--        @if (isset($banner))--}}
 {{--            <a><img src="{{ $banner->fileCustom }}" alt=""></a>--}}
 {{--        @else--}}
