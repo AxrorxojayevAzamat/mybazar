17dd39dc15b390adb3653308dddf0a8b398460ce
azamat <axrorxojayevazamat@gmail.com>
2020-12-11 16:31:43 +0500

    banner show

--- resources/views/layouts/slide-banner-catalog.blade.php
+++ resources/views/layouts/slide-banner-catalog.blade.php
@@ -1,9 +1,13 @@
 <section>
     <div class="slide-banner">
-        @if (isset($banner))
-            <a><img src="{{ $banner->fileCustom }}" alt=""></a>
+        @if ($longBanner)
+            <a href="{{ $longBanner->url }}"><img src="{{ $longBanner->fileCustom }}" alt=""></a>
+        @endif
+{{--        {{dd($banner)}}--}}
+{{--        @if (isset($banner))--}}
+{{--            <a><img src="{{ $banner->fileCustom }}" alt=""></a>--}}
 {{--        @else--}}
 {{--            <img src="{{asset('images/catalog-slider-banner.png')}}" alt="">--}}
-        @endif
+{{--        @endif--}}
     </div>
 </section>
