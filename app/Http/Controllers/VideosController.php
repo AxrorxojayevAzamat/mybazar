b5ced075087acd036da351ddcd4059e8aeaa2faa
Nasriddinbek Bektemirov <n.bektemirov@student.inha.uz>
2020-12-18 09:43:24 +0500

    finishing filter blog store video

--- app/Http/Controllers/VideosController.php
+++ app/Http/Controllers/VideosController.php
@@ -5,16 +5,28 @@ namespace App\Http\Controllers;
 use App\Entity\Category;
 use App\Entity\Blog\Video;
 use App\Entity\Shop\Product;
+use App\Helpers\LanguageHelper;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Request;
 
 class VideosController extends Controller
 {
 
-    public function index()
+    public function index(Request $request)
     {
 
-        $videos = Video::orderByDesc('created_at')->with(['category'])->paginate(20);
-        $categories = Category::orderByDesc('created_at')->get();
+        $videos = Video::published()->orderByDesc('created_at')->with(['category'])->paginate(20);
+        if (isset($request->videoName)){
+            $value = $request->videoName;
+            $videos = Video::search($value)->where('status', Video::PUBLISHED)->paginate(10);
+        }
+        $videosCategoryId = $videos->pluck('category_id')->toArray();
+
+        $categories = Category::whereIn('id', $videosCategoryId)->orderByDesc('created_at')->get();
+        if (isset($request->categoryName)){
+            $videos = $videos->where('category_id', $request->get('categoryName'));
+        }
+
 //        $parentIds = [];
 //        foreach($categories as $i => $category) {
 //            $parentIds[$i] = [
@@ -25,7 +37,7 @@ class VideosController extends Controller
         return view('videoblog.videoblog', compact('videos', 'categories', 'recentProducts'));
     }
 
-    public function show(Video $video)
+    public function show(Video $video, Request $request)
     {
         $video = $video->load(['category']);
         $videos = Video::orderByDesc('created_at')->limit(3)->get();
---
+++ resources/views/filters/category-blog-filter.blade.php
@@ -0,0 +1,29 @@
+@if (isset($categories) || $parentCategory)
+    <button type="button" class="btn accordion active category-btn">@lang('frontend.breadcrumb.categories')</button>
+    <div id="filter2" class="panel">
+
+        <div class="custom-control custom-checkbox">
+            <ul>
+                @if(isset($parentCategory))
+                    @foreach($parentCategory as $i => $category)
+                        <li class="category-list">
+                            <a href="?categoryName={{ $category->id }}"><-{{ $category->name }}</a>
+                        </li>
+                    @endforeach
+                @elseif(isset($rootCategoryShow))
+                    <li class="category-list">
+                        <a href="/categories"><-@lang('menu.whole_catalog')</a>
+                    </li>
+                @endif
+                @if(isset($categories))
+                    @foreach($categories as $i => $category)
+                        <li class="category-list">
+                            <a href="?categoryName={{ $category->id }}">{{ $category->name }}</a>
+                        </li>
+                    @endforeach
+                @endif
+            </ul>
+        </div>
+    </div>
+    <input type="hidden" name="brands" id="brands-hidden-input">
+@endif
