2d10b23e97ed5ed696af3b1f8ba008a9cedad3e4
azamat <axrorxojayevazamat@gmail.com>
2020-12-23 10:47:25 +0500

    fix little changes

--- resources/views/partials/414/header.blade.php
+++ resources/views/partials/414/header.blade.php
@@ -5,10 +5,11 @@
     </div>
 
     <div class="other-icons">
-{{--        <div class="search-button">--}}
-{{--            <a href="#search" class="search-toggle" data-selector="#header-2"></a>--}}
-{{--        </div>--}}
-        <a href="#" class="search"><button class="btn search search-toggle" data-selector="#header-2"><i class="mbsearch"><span></span></button></i> </a>
+        <a href="#" class="search">
+            <button class="btn search search-toggle" data-selector="#header-2">
+                <i class="mbsearch"><span></span></i>
+            </button>
+        </a>
         <a href="{{ route('compare') }}" class="comparison"><i class="mbcompare"><span></span></i> </a>
         <a href="{{ route('cart') }}" class="cart"><i class="mbcart"><span></span></i></a>
         <a href="{{ route('user.favorites') }}" class="wish-list"><i class="mbfavorite"><span></span></i></a>
@@ -54,7 +55,6 @@
                 </form>
             </div>
         @endguest
-{{--        <a href="{{ route('login') }}" class="account"><i class="mbaccount" style="margin: 0"></i></a>--}}
     </div>
     <form action="" class="search-box">
         <input type="text" class="text search-input" placeholder="@lang('frontend.search_on_mybazar')"/>
--- resources/views/partials/414/menu.blade.php
+++ resources/views/partials/414/menu.blade.php
@@ -35,42 +35,24 @@
             </ul>
         </li>
 
-        <li><a href="#">{{ trans('frontend.nav.stock') }} </a></li>
-        <li><a href="#">{{ trans('frontend.nav.discount') }}</a></li>
-{{--        <li>--}}
-{{--            <span>{{trans('frontend.products')}}</span>--}}
-{{--            <ul>--}}
-{{--                <li><a href="#about/history">{{trans('frontend.history')}}</a></li>--}}
-{{--                <li>--}}
-{{--                    <span>{{trans('menu.the_team')}}</span>--}}
-{{--                    <ul>--}}
-{{--                        <li><a href="#about/team/management">{{trans('menu.the_team')}}</a></li>--}}
-{{--                        <li><a href="#about/team/sales">{{trans('menu.sales')}}</a></li>--}}
-{{--                        <li><a href="#about/team/development">{{trans('menu.development')}}</a></li>--}}
-{{--                    </ul>--}}
-{{--                </li>--}}
-{{--                <li><a href="#about/address">{{trans('menu.our_address')}}</a></li>--}}
-{{--            </ul>--}}
-{{--        </li>--}}
         <li><a href="{{ route('brands') }}" class="pn-ProductNav_Link">{{ trans('frontend.nav.top_brands') }}</a></li>
-        <li><a href="{{ route('products.new-products') }}" class="pn-ProductNav_Link">{{ trans('frontend.nav.new_products') }}</a></li>
+        <li>
+            <a href="{{ route('products.new-products') }}"
+               class="pn-ProductNav_Link">{{ trans('frontend.nav.new_products') }}</a>
+        </li>
         <li><a href="{{ route('blogs') }}" class="pn-ProductNav_Link">{{ trans('frontend.nav.blogs') }}</a></li>
-        <li><a href="{{ route('videos.index') }}" class="pn-ProductNav_Link">{{ trans('frontend.nav.videos') }}</a></li>
-        <li><a href="{{ route('discounts.index') }}" class="pn-ProductNav_Link">{{ trans('frontend.nav.discount') }}</a></li>
+        <li>
+            <a href="{{ route('videos.index') }}" class="pn-ProductNav_Link">{{ trans('frontend.nav.videos') }}</a></li>
+        <li>
+            <a href="{{ route('discounts.index') }}" class="pn-ProductNav_Link">{{ trans('frontend.nav.discount') }}</a>
+        </li>
         <li><a href="{{ route('stores.index') }}" class="pn-ProductNav_Link">{{ trans('frontend.nav.shops') }}</a></li>
         <li><a href="{{ route('pay') }}" class="pn-ProductNav_Link">{{ trans('frontend.nav.pay') }}</a></li>
         <li><a href="{{ route('delivery') }}" class="pn-ProductNav_Link">{{ trans('frontend.nav.delivery') }}</a></li>
 
-{{--        <li><a href="#">{{ trans('frontend.nav.top_brands') }}</a></li>--}}
-{{--        <li><a href="#">{{ trans('frontend.nav.blogs') }}</a></li>--}}
-{{--        <li><a href="#">{{ trans('frontend.nav.videos') }}</a></li>--}}
-{{--        <li><a href="#">{{ trans('frontend.nav.delivery') }}</a></li>--}}
-{{--        <li><a href="#">{{ trans('frontend.nav.business') }}</a></li>--}}
-{{--        <li><a href="#">{{ trans('frontend.nav.shops') }}</a></li>--}}
-{{--        <li><a href="#">{{ trans('frontend.nav.pay') }}</a></li>--}}
         <li class="menu-tel-mail">
-            <a href="tel:" class="bold tel">+998 92 123 45 67</a>
-            <a href="info@mybazar.com" class="email" target="_blank">info@mybazar.com</a>
+            <a href="tel:+998921234567" class="bold tel">+998 92 123 45 67</a>
+            <a href="mailto:info@mybazar.com" class="email" target="_blank">info@mybazar.com</a>
         </li>
     </ul>
 </nav>
