f71971da1d28f0be52a955c20fc36643f17bd17e
azamat <axrorxojayevazamat@gmail.com>
2020-12-24 15:01:47 +0500

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,10 +31,9 @@ class AppServiceProvider extends ServiceProvider
             $gBrands = Brand::get();
             $discountProducts = Product::where('discount', '>', 0.5 )->limit(3)->get();
             $pages = Page::get();
-            if (!Banner::published()->get()->isEmpty()) {
-                $longBanner = Banner::published()->where('type', Banner::TYPE_LONG)->get()->random();
-            }
+            $longBanner = Banner::published()->where('type', Banner::TYPE_LONG)->get()->random();
 
+//            dd($pages[0]->children);
             if ($gUserExists !== null){
                 $gCartCount = Cart::where('user_id', $gUserExists->id)->get();
             }
